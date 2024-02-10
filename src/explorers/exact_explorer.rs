670e0ba9298dbaf0e3c77d9aa59b223146645f88
Justin <justin.gray@reeldata.ai>
2024-02-10 00:29:08 -0400

    improved imports

--- src/explorers/exact_explorer.rs
+++ src/explorers/exact_explorer.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::path::Path;
-use crate::explorers::Explorer;
+use crate::explorers::{Explorer, HashMap, SystemTime};
 
 
 pub struct ExactExplorer {
@@ -9,7 +9,7 @@ pub struct ExactExplorer {
 
 
 impl Explorer for ExactExplorer {
-    fn explore(&self, files: &mut std::collections::HashMap<String, std::time::SystemTime>) {
+    fn explore(&self, files: &mut HashMap<String, SystemTime>) {
         if let Ok(metadata) = fs::metadata(&self.path) {
             let mtime = metadata.modified().expect("mtime is not supported on your platform");
             files.insert(self.path.to_string_lossy().to_string(), mtime);
--- src/explorers/mod.rs
+++ src/explorers/mod.rs
@@ -1,10 +1,11 @@
 mod exact_explorer;
 
+pub use std::collections::HashMap;
+pub use std::time::SystemTime;
 pub use exact_explorer::ExactExplorer;
-use std::collections::HashMap;
 
 
 pub trait Explorer {
     /// Explore the file system for file path(s) matching the pattern
-    fn explore(&self, files: &mut HashMap<String, std::time::SystemTime>);
+    fn explore(&self, files: &mut HashMap<String, SystemTime>);
 }
