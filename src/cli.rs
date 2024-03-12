39fa3795a368db803d65c789b0d630196c03a844
Justin <justin.gray@reeldata.ai>
2024-03-12 00:14:44 -0300

    checkpoint

--- src/cli.rs
+++ src/cli.rs
@@ -7,6 +7,7 @@ use clap::{ArgAction, Parser};
 Justin's file system watching program.
 
 When some path of interest on the file system changes, run a specified command.
+[Project repository on GitHub](https://github.com/just1ngray/jfswatch).
 
 ## About
 
@@ -16,34 +17,37 @@ file changes every `interval` seconds. If a change is detected, the program
 will execute the specified command and sleep for `sleep` seconds before
 resuming standard interval checks.
 
-The logging level can be changed by setting the `RUST_LOG` environment variable
-to one of: `trace`, `debug`, `info`, `warn`, `error`.
-
 ## Examples
 
 ### Simple Example
 
-Run `cargo test` when any Rust file changes. Check for changes every 0.5
-seconds and sleep for 2.0 seconds after running the tests.
+Restart the `my-program.service` systemd service when any configuration file
+inside `/etc/my-program` changes, or when the binary used by the service is
+updated.
+
+JFSWatch will check for changes every 0.5 seconds, and sleep for 10 seconds
+after restarting the service.
 
 ```shell
 $ jfswatch \
     --interval 0.5 \
-    --sleep 2.0 \
-    --glob '**/*.rs' \
-    --exact Cargo.toml \
-    cargo test
+    --sleep 10.0 \
+    --glob '/etc/my-program/**' \
+    --exact /usr/bin/my-program \
+    systemctl restart my-program.service
 ```
+
+> Note: for restarting systemd services, you can create a corresponding path
+> unit which will automatically restart the service when the specified path(s)
+> are updated. This is probably more efficient, but if you care about
+> flexibility, observability, and ease-of-use, then jfswatch will help you move
+> faster with more confidence.
 
 ### Full Shell Example
 
 When you want to use powerful shell features such as pipes (`|`), redirects
 (`>`), multiple commands (`&&`), or environment variables, you must quote your
 command.
-
-For example, each time `Cargo.toml` is modified, append the current date to a
-file called `Cargo.toml_was_modified.txt` and print the `$SHELL` environment
-variable used to execute that command.
 
 Note the difference between running `"echo $SHELL"` and `'echo $SHELL'`. When
 double quoted, `$SHELL` will be evaluated first and then passed into jfswatch.
@@ -52,11 +56,26 @@ evaluated later when the command is run. This difference is reflected in the
 jfswatch logs. For this reason it is recommended to use single quotes when
 using all shell features, or substitution variables in the command.
 
+The following example will overwrite the contents of the README with the cli's
+help documentation, which proved useful while updating the documentation.
+
 ```shell
 $ jfswatch \
+    --glob '**/*.rs' \
     --exact Cargo.toml \
-    'echo running command in $SHELL && echo $(date) >> Cargo.toml_was_modified.txt'
+    'cargo run -- -h > README.md'
 ```
+
+## Extras
+
+- It's usually best to use single quotes when accessing full shell features.
+  Otherwise the shell will evaluate substituted variables like `$diff` before
+  jfswatch can use them
+- Be careful not to create a loop where jfswatch watches a file that is
+  modified by the command it runs. The logs will make this obvious if this
+  happens, but it can still be an annoying mistake to make
+- The logging level can be changed by setting the `RUST_LOG` environment
+  variable to one of: `trace`, `debug`, `info`, `warn`, `error`
 
 ## Usage
 ```"#)]
@@ -79,9 +98,9 @@ pub struct Cli {
     pub sleep: Option<f32>,
 
     /// The command to execute when changes are detected. The command can include substitutable bash-like variables:
-    /// `$diff` or `${diff}` will be one of `new`, `deleted`, or `modified` according to the detected change.
-    /// `$path` or `${path}` will be the watched path that changed.
-    /// `$mtime` or `${mtime}` will be the last modified time of the watched path (unavailable for deleted paths).
+    /// - `$diff` or `${diff}` will be one of `new`, `deleted`, or `modified` according to the detected change.
+    /// - `$path` or `${path}` will be the watched path that changed.
+    /// - `$mtime` or `${mtime}` will be the last modified time of the watched path (unavailable for deleted paths).
     #[arg(required = true)]
     pub cmd: Vec<String>,
 }
